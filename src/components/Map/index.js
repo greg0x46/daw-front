125fb31644ed332b068909a7c771f8ec87d3c925
matheus.sampaio <matheussampaiomedeiros@gmail.com>
2020-08-08 16:03:42 -0300

    uhul2

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import L from 'leaflet';
+import React from 'react'
+import { Map as LeafletMap, GeoJSON, Marker, Popup } from 'react-leaflet';
+import mapadusguri from 'ONS_UHE.geojson';
 
-class Map extends React.Component {
-  componentDidMount() {
-    // create map
-    this.map = L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
-    });
-  }
-
+class GeoJsonMap extends React.Component {
   render() {
-    return <div id="map" style={{height: "calc(100vh - 64px)"}}></div>;
+    return (
+      <LeafletMap
+        center={[49.8419, 24.0315]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <GeoJSON
+          data={mapadusguri}
+          style={() => ({
+            color: '#4a83ec',
+            weight: 0.5,
+            fillColor: "#1a1d62",
+            fillOpacity: 1,
+          })}
+        />
+        <Marker position={[49.8419, 24.0315]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    
+    );
   }
 }
 
-export default Map;
+
+export default GeoJsonMap;
