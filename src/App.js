d3d556a2028f70b1f540e93ef227051c4fef712f
matheus.sampaio <matheussampaiomedeiros@gmail.com>
2020-08-08 19:04:10 -0300

    agrvai1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Base from './pages/Base'
 import Map from './components/Map'
-import GeoJsonMap from './components/Map'
+
 function App(){
   return (
     <Base>
       <Map>
-        <GeoJsonMap>
-        </GeoJsonMap>
+
         </Map>
     </Base>
   );
